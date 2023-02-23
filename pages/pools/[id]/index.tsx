335ef6f10866f2a7d2aede53d6fc45c24e824e3a
Danijel-Enoch <enochdanijel@gmail.com>
2023-02-23 20:43:36 +0100

    updated

--- pages/pools/[id]/index.tsx
+++ pages/pools/[id]/index.tsx
@@ -2,55 +2,13 @@ import HomeStyles from "../../../styles/Home.module.css";
 import styles from "../../../styles/Launchpad.module.css";
 import Head from "next/head";
 import {server} from "../../../config/index";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Navbar from "../../../components/Navbar/Navbar";
 import AboutProject from "../../../components/PoolCardDetails/AboutProject";
 import HeroSection from "../../../components/PoolCardDetails/HeroSection";
 import PoolInformation from "../../../components/PoolCardDetails/PoolInformation";
 import TokenInformation from "../../../components/PoolCardDetails/TokenInformation";
-import { GlobalAuth } from "../../../context/GlobalContext";
-
-// export const getStaticPaths = async () => {
-//   console.log(server)
-//   const res = await fetch(`${server}/api/pools`);
-//   const data = await res.json();
-
-//   // const ids = pool.map((item : any) => item.id);
-//   const paths = data.map((item: any) => {
-//     return {
-//       params: {
-//         id: item.id.toString(),
-//       },
-//     };
-//   });
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-// export const getStaticProps = async (context: any) => {
-//   const id = context.params.id;
-//   const res = await fetch(`${server}/api/pools/${id}`);
-//   const data = await res.json();
-//   console.log(data);
-
-//   return {
-//     props: {
-//       pool: data,
-//     },
-//   };
-// };
 
 const Pool = ({pool} : any) => {
-  // const { poolsData } = GlobalAuth();
-  // const router = useRouter();
-  // const { id } = router.query;
-  
-  // const [pool, setPool] = useState();
-
   return (
     <div className={`${HomeStyles.container} ${styles.poolCardDetails}`}>
       <Head>
