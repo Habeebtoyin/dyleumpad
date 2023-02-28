70e6749639e58548e02c5b1dece1c22a55087ad2
Codexjay <jaydencolins08@gmail.com>
2023-02-28 12:46:17 +0100

    added another 'buy' button

--- components/LaunchPad/BuyButtons.jsx
+++ components/LaunchPad/BuyButtons.jsx
@@ -1,14 +1,16 @@
-import border from "../assets/icons/buy-btn-border.svg";
 import styles from '../../styles/Launchpad.module.css';
 
 export default function BuyButtons() {
   return (
     <section className={styles.buyBtns}>
-      <a href="#" className={`${styles.buyBtn} ${styles.buyBtnActive}`}>
+      <a href="https://equalizer.exchange/swap?outputCurrency=0x39263a476aadf768be43a99b24c4e461098524a4" target="_blank" rel='noreferrer' className={`${styles.buyBtn} ${styles.buyBtnActive}`}>
         Buy on Equaliser
       </a>
-      <a href="#" className={`${styles.buyBtn} `}>
-        Buy on Firebird
+      <a href="https://app.velodrome.finance/swap?outputCurrency=0xd652776de7ad802be5ec7bebfafda37600222b48" target="_blank" rel='noreferrer' className={`${styles.buyBtn} `}>
+        Buy on Velodrome
+      </a>
+      <a href="https://thena.fi/swap?outputCurrency=0xeba457b55fb145ff4451bc50fb6c373e5caa493f" target="_blank" rel='noreferrer' className={`${styles.buyBtn} `}>
+        Buy on Thenafi
       </a>
     </section>
   );
