c4393cf9a770638ab6b17e82afd7f81ae319b29e
azarems <azeezaremu708@gmail.com>
2023-09-12 14:06:40 +0100

    update here

--- pages/api/pools/[id].ts
+++ pages/api/pools/[id].ts
@@ -1,11 +1,11 @@
-import { ActivePools } from "../../../data/PoolsData";
+// import { ActivePools } from "../../../data/PoolsData";
 
-export default async function handler({ query: { id } }:any, res:any) {
-  const filtered = ActivePools?.filter((pool) => pool.id === Number(id));
+// export default async function handler({ query: { id } }:any, res:any) {
+//   const filtered = ActivePools?.filter((pool) => pool.id === Number(id));
 
-  if (filtered?.length > 0) {
-    res.status(200).json(filtered[0]);
-  } else {
-    res.status(404).json({ message: `Pool with the id of ${id} is not found` });
-  }
-}
+//   if (filtered?.length > 0) {
+//     res.status(200).json(filtered[0]);
+//   } else {
+//     res.status(404).json({ message: `Pool with the id of ${id} is not found` });
+//   }
+// }
